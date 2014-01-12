ff1594af48b26f8d7c9dcc626acf01cf30d0e357
phesto <dledford@gmail.com>
2014-01-12 00:34:37 -0500

    formatting

--- SpaceRockBlast/src/space/Bullet.java
+++ SpaceRockBlast/src/space/Bullet.java
@@ -7,12 +7,12 @@ import static util.JGameConstants.PLAYERWIDTH;
 import entities.AbstractMoveableEntity;
 import org.lwjgl.opengl.GL11;
 
-public class Bullet extends AbstractMoveableEntity{
-	
+public class Bullet extends AbstractMoveableEntity {
+
 	protected double rotate = 0;
-	
+
 	protected int id;
-	
+
 	public int getId() {
 		return id;
 	}
@@ -29,57 +29,59 @@ public class Bullet extends AbstractMoveableEntity{
 		this.rotate = rotate;
 	}
 
-	public Bullet(double x, double y, double width, double height, double rotate, int id) {
+	public Bullet(double x, double y, double width, double height,
+			double rotate, int id) {
 		super(x, y, width, height);
-		this.id = id+1;
+		this.id = id + 1;
 		this.speed = .5f;
 		// TODO Auto-generated constructor stub
 		this.rotate = rotate;
 	}
-	
-	public boolean onScreen(){				
-		if(this.x>(DISPLAYWIDTH+2)){
+
+	public boolean onScreen() {
+		if (this.x > (DISPLAYWIDTH + 2)) {
 			return false;
-		}else if(this.x<-2){
+		} else if (this.x < -2) {
 			return false;
 		}
-		if(this.y>(DISPLAYHEIGHT+2)){
+		if (this.y > (DISPLAYHEIGHT + 2)) {
 			return false;
-		}else if(this.y<-2){
+		} else if (this.y < -2) {
 			return false;
 		}
 		return true;
 	}
-	
-		
+
 	@Override
 	public void draw() {
 		GL11.glLoadIdentity();
-		GL11.glColor3f(1,1,1);
+		GL11.glColor3f(1, 1, 1);
 		GL11.glPushMatrix();
-	    GL11.glTranslated(this.x,this.y, 0);
-	    GL11.glRotated(rotate, 0, 0, 1);
+		GL11.glTranslated(this.x, this.y, 0);
+		GL11.glRotated(rotate, 0, 0, 1);
 		GL11.glBegin(GL11.GL_QUADS);
 		{
-		
-			GL11.glVertex3d(-(this.width/2),-(this.height/2),0);	
-			GL11.glVertex3d((this.width/2),-(this.height/2),0);
-			GL11.glVertex3d((this.width/2),(this.height/2),0);
-			GL11.glVertex3d(-(this.width/2),(this.height/2),0);		
-			//GL11.glRectd(this.x, this.y, this.x+this.width, this.y+this.height);
+
+			GL11.glVertex3d(-(this.width / 2), -(this.height / 2), 0);
+			GL11.glVertex3d((this.width / 2), -(this.height / 2), 0);
+			GL11.glVertex3d((this.width / 2), (this.height / 2), 0);
+			GL11.glVertex3d(-(this.width / 2), (this.height / 2), 0);
+			// GL11.glRectd(this.x, this.y, this.x+this.width,
+			// this.y+this.height);
 		}
-		GL11.glEnd();	
-		//GL11.glTranslated(-DISPLAYWIDTH,-DISPLAYHEIGHT, 0);
+		GL11.glEnd();
+		// GL11.glTranslated(-DISPLAYWIDTH,-DISPLAYHEIGHT, 0);
 		GL11.glPopMatrix();
-				
-	}
-	
-	@Override
-	public boolean equals(Object o) {
-	    if(!(o instanceof Bullet)) return false;
-	    Bullet other = (Bullet) o;
-	    return (this.x == other.getX() && this.y == other.getY() && this.id == other.getId());
+
 	}
 
-		
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof Bullet))
+			return false;
+		Bullet other = (Bullet) o;
+		return (this.x == other.getX() && this.y == other.getY() && this.id == other
+				.getId());
+	}
+
 }
